027886d948b4f072182de44f657793d6cc704583
Daniel Rieck <danielcrieck@gmail.com>
2017-10-21 21:27:49 -0700

    Updating backup of latest verified custom JS build

---
+++ JS/oue-custom.min.prev.js
@@ -0,0 +1,8 @@
+﻿This site can’t be reached
+
+oue.wsu.edu unexpectedly closed the connection.
+Try:
+Checking the connection
+Checking the proxy and the firewall
+Running Windows Network Diagnostics
+ERR_CONNECTION_CLOSED
