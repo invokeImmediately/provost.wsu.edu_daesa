c3b8beba09d7eaa4e440c8514fd2464258ec3854
Daniel Rieck, Ph.D <danielcrieck@gmail.com>
2018-11-25 20:43:34 -0800

    Update gulp build processes
    Update gulp CSS build process to latest version involving dependency WSU-UE---JS/gulpBuilder.js, and add JS build process.

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,79 @@
-var compiledCssSrcFileName = 'oue-custom.css',
-	minCssFileHeaderStr = '/* Built with the LESS CSS preprocessor [http://lesscss.org/]. Please see [https://github.com/invokeImmediately/oue.wsu.edu] for a repository of source code. */\r\n',
-	gulp = require('gulp'),
-	lessc = require('gulp-less'),
-	gcmq = require('gulp-group-css-media-queries'),
-	insertLines = require('gulp-insert-lines'),
-	cleanCss = require('gulp-clean-css'),
-	insert = require('gulp-insert'),
-	extName = require('gulp-extname');
+'use strict';
 
-gulp.task('buildMinCss', function () {
-	gulp.src('./CSS/*.less')
-		.pipe(lessc({
-			paths: ['./WSU-UE---CSS/']
-		}))
-		.pipe(gulp.dest('./CSS/'))
-		.pipe(gcmq())
-		.pipe(insertLines({
+/* -------------------------------------------------------------------------------------------------
+** Variable Declarations
+*/
+
+// Gulp task dependencies
+var gulpBuilder = require( './WSU-UE---JS/gulpBuilder.js' );
+
+/* -------------------------------------------------------------------------------------------------
+** Function declarations
+*/
+
+/**
+ * Get the settings for a gulp-mediated custom CSS build from Less source files.
+ *
+ * @return {object} - Instance of gulpBuilder.CssBuildSettings.
+ */
+function getCssBuildSettings() {
+	var commentRemovalNeedle = /^(?:[ \t]*)?\/\*[^!].*$\n(?:^\*\*?[^/].*$\n)*\*\*?\/\n\n?/gm;
+	var dependenciesPath = './WSU-UE---CSS/';
+	var destFolder = './CSS/';
+	var fontImportStr = '@import url(\'https://fonts.googleapis.com/css?family=Roboto+Mono:400,700|\
+Roboto+Condensed:400,700|Roboto+Slab|PT+Serif\');\r\n';
+	var insertingMediaQuerySectionHeader = {
 			'before': /^@media/,
-			'lineBefore': '/*! ╔═══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════╗\r\n*   ║ MEDIA QUERIES ####################################################################################################### ║\r\n*   ╚═══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════╝\r\n*/',
+			'lineBefore': '/*! ╔═══════════════════════════════════════════════════════════════════\
+════════════════════════════════════════════════════╗\r\n*   ║ MEDIA QUERIES ######################\
+################################################################################# ║\r\n*   ╚═══════\
+═══════════════════════════════════════════════════════════════════════════════════════════════════\
+═════════════╝\r\n*/',
 			'stopAfterFirstMatch': true
-		}))
-		.pipe(cleanCss())
-		.pipe(insert.prepend(minCssFileHeaderStr))
-		.pipe(extName('.min.css'))
-		.pipe(gulp.dest('./CSS/'));
-});
+		};
+	var minCssFileExtension = '.min.css';
+	var minCssFileHeaderStr = '/*! Built with the Less CSS preprocessor [http://lesscss.org/]. Plea\
+se see [https://github.com/invokeImmediately/oue.wsu.edu] for a repository of source code. */\r\n';
+ 	var sourceFile = './CSS/oue-custom.less';
+
+	return new gulpBuilder.CssBuildSettings(commentRemovalNeedle, dependenciesPath,
+ 		destFolder, fontImportStr, insertingMediaQuerySectionHeader, minCssFileExtension,
+ 		minCssFileHeaderStr, sourceFile);
+}
+
+/**
+ * Get the settings for a gulp-mediated custom JS build.
+ *
+ * @return {object} - Simple collection of settings for JS builds.
+ */
+function getJsBuildSettings() {
+	return {
+		buildDependenciesList: [
+			'./WSU-UE---JS/jQuery.oue-custom.js',
+			'./WSU-UE---JS/jQuery.oue-animate.js',
+			'./WSU-UE---JS/jQuery.cookieObjs.js',
+			'./WSU-UE---JS/jQuery.forms.js',
+			'../jQuery.AreYouSure/jquery.are-you-sure.js',
+			'./WSU-UE---JS/jQuery.are-you-sure.js',
+			'../qTip2/dist/jquery.qtip.min.js',
+			'./WSU-UE---JS/jQuery.qTip.js',
+			'./WSU-UE---JS/jQuery.textResize.js',
+			'../imagesloaded/imagesloaded.pkgd.min.js',
+			'../masonry/dist/masonry.pkgd.min.js',
+			'./WSU-UE---JS/jQuery.masonry-custom.js',
+			'./JS/oue-custom.js'
+		],
+		commentNeedle: /^(\/\*)(?!!)/g,
+		compiledJsFileName: 'oue-build.js',
+		destFolder: './JS/',
+		minJsFileExtension: '.min.js',
+		replaceCallback: gulpBuilder.fixFileHeaderComments
+	};
+}
+
+/* -------------------------------------------------------------------------------------------------
+** Main execution sequence
+*/
+
+gulpBuilder.setUpCssBuildTask( getCssBuildSettings() );
+gulpBuilder.setUpJsBuildTask( getJsBuildSettings() );
