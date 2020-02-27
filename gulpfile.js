81621469a2a739647c6ba43737fd54eb4b5f5533
Daniel Rieck, Ph.D <danielcrieck@gmail.com>
2020-02-27 10:27:06 -0800

    Update inline documentation, CSS build settings

--- gulpfile.js
+++ gulpfile.js
@@ -2,22 +2,25 @@
  * gulpfile.js
  * -------------------------------------------------------------------------------------------------
  * SUMMARY: Gulp automation task definition file for setting up tasks that build CSS and JS
- * files for use on the WSUWP website of the WSU Office of Undergraduate Education.
+ * files for use on the WSUWP website of the Division of Academic Engagement and Student Achievement
+ * (DAESA) at Washington State University (WSU).
+ *
+ * DESCRIPTION: This gulp automation task definition file is designed for use on the following
+ *   project that is maintained on GitHub:
+ *   https://github.com/invokeImmediately/oue.wsu.edu
  *
  * AUTHOR: Daniel Rieck [daniel.rieck@wsu.edu] (https://github.com/invokeImmediately)
  *
- * REPOSITORY: https://github.com/invokeImmediately/nsse.wsu.edu
- *
- * LICENSE: ISC - Copyright (c) 2019 Daniel C. Rieck.
+ * LICENSE: ISC - Copyright (c) 2020 Daniel C. Rieck.
  *
  *   Permission to use, copy, modify, and/or distribute this software for any purpose with or
  *   without fee is hereby granted, provided that the above copyright notice and this permission
  *   notice appear in all copies.
  *
- *   THE SOFTWARE IS PROVIDED "AS IS" AND DANIEL RIECK DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS
- *   SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL
- *   DANIEL RIECK BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY
- *   DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF
+ *   THE SOFTWARE IS PROVIDED "AS IS" AND DANIEL C. RIECK DISCLAIMS ALL WARRANTIES WITH REGARD TO
+ *   THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT
+ *   SHALL DANIEL C. RIECK BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR
+ *   ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF
  *   CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
  *   PERFORMANCE OF THIS SOFTWARE.
  **************************************************************************************************/
@@ -56,40 +59,43 @@ function getCssBuildSettings() {
 	var commentRemovalNeedle = /^(?:[ \t]*)?\/\*[^!].*$\n(?:^\*\*?[^/].*$\n)*\*\*?\/\n\n?/gm;
 	var dependenciesPath = './WSU-UE---CSS/';
 	var destFolder = './CSS/';
-	var fontImportStr = '@import url(\'https://fonts.googleapis.com/css?family=Open+Sans:300,300i,4\
-00,400i,700,700i|Roboto+Condensed:400,400i,700,700i|PT+Serif:400,400i,700,700i|Roboto+Mono:400,400i\
-,700,700i&display=swap\');\r\n';
+	var fontImportStr = '@import url(\'https://fonts.googleapis.com/css?family=Open+Sans:300,300i' +
+		',400,400i,600,600i,700,700i|Roboto+Condensed:400,400i,700,700i|PT+Serif:400,400i,700,700' +
+		'i|Roboto+Mono:400,400i,700,700i&display=swap\');\r\n';
 	var insertingMediaQuerySectionHeader = {
-			'before': /^@media/,
-			'lineBefore': '/*! ====================================================================\
-============================\r\n*** Media queries section\r\n*** ==================================\
-==============================================================\r\n***   SUMMARY: Media queries buil\
-t from precompiled CSS written in the Less language extension of\r\n***    CSS. Queries in this sec\
-tion are a combination of those designed for use on websites belonging to DAESA ***\r\n    and thos\
-e intended specifically for use on the DAESA website.\r\n***\r\n***   DESCRIPTION: Fully documented\
-, precompiled source code from which this section of the custom\r\n***    stylesheet was built is d\
-eveloped and maintained on the following two GitHub projects:\r\n***    https://github.com/invokeIm\
-mediately/WSU-UE---CSS/\r\n***    https://github.com/invokeImmediately/oue.wsu.edu/\r\n***   AUTHOR\
-: Daniel Rieck [daniel.rieck@wsu.edu] (https://github.com/invokeImmediately)\r\n***\r\n***   LICENS\
-E: ISC - Copyright (c) 2019 Daniel C. Rieck.\r\n***\r\n***     Permission to use, copy, modify, and\
-/or distribute this software for any purpose with or\r\n***     without fee is hereby granted, prov\
-ided that the above copyright notice and this permission\r\n***     notice appear in all copies.\r\
-\n***\r\n***     THE SOFTWARE IS PROVIDED "AS IS" AND DANIEL RIECK DISCLAIMS ALL WARRANTIES WITH RE\
-GARD TO\r\n***     THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. I\
-N NO EVENT\r\n***     SHALL DANIEL RIECK BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENT\
-IAL DAMAGES OR\r\n***     ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETH\
-ER IN AN ACTION OF\r\n***     CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN C\
-ONNECTION WITH THE USE\r\n***     OR PERFORMANCE OF THIS SOFTWARE.\r\n*** =========================\
-=======================================================================\r\n**/',
-			'stopAfterFirstMatch': true
-		};
+		'before': /^@media/,
+		'lineBefore': '/*! ======================================================================' +
+			'==========================\r\n*** Media queries section\r\n*** =====================' +
+			'===========================================================================\r\n*** S' +
+			'UMMARY: Media queries built from precompiled CSS written in the Less language extens' +
+			'ion of\r\n***   CSS. Queries in this section are a combination of those designed for' +
+			' use on all websites\r\n***   managed by WSU DAESA and those intended specifically f' +
+			'or use on the DAESA website.\r\n***\r\n*** DESCRIPTION: Fully documented, precompile' +
+			'd source code from which this section of the custom\r\n***   stylesheet was built is' +
+			' developed and maintained on the following two GitHub projects:\r\n***     https://g' +
+			'ithub.com/invokeImmediately/WSU-UE---CSS/\r\n***     https://github.com/invokeImmedi' +
+			'ately/oue.wsu.edu/\r\n***\r\n*** AUTHOR: Daniel Rieck [daniel.rieck@wsu.edu] (https:' +
+			'//github.com/invokeImmediately)\r\n***\r\n*** LICENSE: ISC - Copyright (c) 2020 Dani' +
+			'el C. Rieck.\r\n***\r\n***   Permission to use, copy, modify, and/or distribute this' +
+			' software for any purpose with or\r\n***   without fee is hereby granted, provided t' +
+			'hat the above copyright notice and this permission\r\n***   notice appear in all cop' +
+			'ies.\r\n***\r\n***   THE SOFTWARE IS PROVIDED "AS IS" AND DANIEL C. RIECK DISCLAIMS ' +
+			'ALL WARRANTIES WITH REGARD TO\r\n***   THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIE' +
+			'S OF MERCHANTABILITY AND FITNESS. IN NO EVENT\r\n***   SHALL DANIEL C. RIECK BE LIAB' +
+			'LE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR\r\n***   ANY DAMAG' +
+			'ES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF\r' +
+			'\n***   CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTI' +
+			'ON WITH THE USE\r\n***   OR PERFORMANCE OF THIS SOFTWARE.\r\n*** ===================' +
+			'=============================================================================\r\n**/',,
+		'stopAfterFirstMatch': true
+	};
 	var minCssFileExtension = '.min.css';
 	var minCssFileHeaderStr = '';
- 	var sourceFile = './CSS/oue-custom.less';
+	var sourceFile = './CSS/oue-custom.less';
 
 	return new gulpBuilder.CssBuildSettings(commentRemovalNeedle, dependenciesPath,
- 		destFolder, fontImportStr, insertingMediaQuerySectionHeader, minCssFileExtension,
- 		minCssFileHeaderStr, sourceFile);
+		destFolder, fontImportStr, insertingMediaQuerySectionHeader, minCssFileExtension,
+		minCssFileHeaderStr, sourceFile);
 }
 
 ////////
